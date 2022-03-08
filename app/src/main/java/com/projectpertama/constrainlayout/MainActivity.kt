481a2a92befacbfc4e4cb95f3690edc0131697c5
Andre <andreashappy29@gmail.com>
2022-03-08 21:08:38 +0700

    test pertama

---
+++ app/src/main/java/com/projectpertama/constrainlayout/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.projectpertama.constrainlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//
+//
+//        val button = findViewById<Button>(R.id.btnSignUp)
+//        val intent = Intent(this,SignUpActivity::class.java)
+//
+//        button.setOnClickListener{
+//            startActivity(intent)
+//        }
+    }
+}
---
+++ app/src/main/java/com/projectpertama/constrainlayout/SignUpActivity.kt
@@ -0,0 +1,22 @@
+package com.projectpertama.constrainlayout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+//
+//        var button = findViewById<Button>(R.id.btnSignUp)
+//
+//        var goMainActivity = Intent(this,MainActivity::class.java)
+//
+//        button.setOnClickListener {
+//            startActivity(goMainActivity)
+//        }
+    }
+}
